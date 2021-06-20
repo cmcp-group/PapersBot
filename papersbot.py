87bb6a84686327b74f56b2853de1245459fe2e70
Yongchul Chung <chung@northwestern.edu>
2021-06-21 00:10:34 +0900

    Update papersbot.py

--- papersbot.py
+++ papersbot.py
@@ -310,16 +310,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
